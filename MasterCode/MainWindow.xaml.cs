bb66fca3c795221c6ce37c63fbf6edb085940120
LaoyaoCode <LaoyaoCode@outlook.com>
2017-11-29 23:50:00 +0800

    修改了chart scroll bar left right 样式，转化成了圆形，下载了MahUI

--- MasterCode/MainWindow.xaml.cs
+++ MasterCode/MainWindow.xaml.cs
@@ -15,94 +15,20 @@ using System.Windows.Shapes;
 using Visifire.Charts;
 using System.Threading;
 using System.Windows.Threading;
+using MahApps.Metro.Controls;
 
 namespace MasterCode
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
-        // Create new DataSeries
-        DataSeries dataSeries = new DataSeries();
+
 
         public MainWindow()
         {
             InitializeComponent();
-
-            // Add DataSeries to Chart
-            TestChar.Series.Add(dataSeries);
-
-            CreateChart();
-            CreateChart();
-            CreateChart();
-            CreateChart();
-            CreateChart();
-            CreateChart();
-            CreateChart();
-            CreateChart();
-            CreateChart();
-
-
-
-
-            //CreateChart2();
-
-            //CreateChart2();
-        }
-
-
-        private void CreateChart()
-        {
-            dataSeries.RenderAs = RenderAs.Spline;
-
-            // Number of DataPoints to be generated
-            int numberOfDataPoints = 10;
-
-            // To set the YValues of DataPoint
-            Random random = new Random();
-
-            // Loop and add a few DataPoints
-            for (int loopIndex = 0; loopIndex < numberOfDataPoints; loopIndex++)
-            {
-                // Create a DataPoint
-                DataPoint dataPoint = new DataPoint();
-
-                // Set the YValue using random number
-                dataPoint.YValue = random.Next(1, 100);
-
-                // Add DataPoint to DataSeries
-                dataSeries.DataPoints.Add(dataPoint);
-            }
-        }
-
-        private void CreateChart2()
-        {
-            DataSeries dataSeries1 = new DataSeries();
-
-            dataSeries1.RenderAs = RenderAs.Spline;
-
-            // Number of DataPoints to be generated
-            int numberOfDataPoints = 15;
-
-            // To set the YValues of DataPoint
-            Random random = new Random();
-
-            // Loop and add a few DataPoints
-            for (int loopIndex = 0; loopIndex < numberOfDataPoints; loopIndex++)
-            {
-                // Create a DataPoint
-                DataPoint dataPoint = new DataPoint();
-
-                // Set the YValue using random number
-                dataPoint.YValue = random.Next(1, 100);
-
-                // Add DataPoint to DataSeries
-                dataSeries1.DataPoints.Add(dataPoint);
-            }
-
-            // Add DataSeries to Chart
-            TestChar.Series.Add(dataSeries1);
         }
     }
 }
