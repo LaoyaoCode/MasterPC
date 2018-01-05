d8eb0f352f9edf2ad057f22d3a92ed9a78dd8635
LaoyaoCode <LaoyaoCode@outlook.com>
2018-01-05 18:14:17 +0800

    datas界面初步搭建完成

--- MasterCode/MControls/DatasDisplayPage.xaml.cs
+++ MasterCode/MControls/DatasDisplayPage.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MasterCode.MControls;
+using Visifire.Charts;
 
 namespace MasterCode.MControls
 {
@@ -19,9 +21,70 @@ namespace MasterCode.MControls
     /// </summary>
     public partial class DatasDisplayPage : UserControl
     {
+        private ImageTextButton NowSelectDeviceButton = null;
+        private int NowSelectDeviceID = 0;
+
         public DatasDisplayPage()
         {
             InitializeComponent();
+
+            for (int counter = 1; counter <= 20; counter++)
+            {
+                ImageTextButton deviceButton = new ImageTextButton();
+                deviceButton.Style = (Style)this.Resources["MComSelectTButtonStyle"];
+                deviceButton.SetIDAndClick(DeviceSelectButtonClick, counter.ToString());
+                deviceButton.MButtonText = counter.ToString();
+                deviceButton.ButtonText.TextAlignment = TextAlignment.Center;
+                DeviceIDSelectSP.Children.Add(deviceButton);
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        /*
+        private void CreateChart()
+        {
+            
+            DataSeries dataSeries = new DataSeries();
+
+            DTotalChart.Series.Add(dataSeries);
+
+            dataSeries.RenderAs = RenderAs.Spline;
+
+            // Number of DataPoints to be generated
+            int numberOfDataPoints = 10;
+
+            // To set the YValues of DataPoint
+            Random random = new Random();
+
+            // Loop and add a few DataPoints
+            for (int loopIndex = 0; loopIndex < numberOfDataPoints; loopIndex++)
+            {
+
+                // Create a DataPoint
+                DataPoint dataPoint = new DataPoint();
+
+                dataPoint.XValue = loopIndex;
+                // Set the YValue using random number
+                dataPoint.YValue = random.Next(1, 100);
+
+                // Add DataPoint to DataSeries
+                dataSeries.DataPoints.Add(dataPoint);
+            }
+        }*/
+
+        private void DeviceSelectButtonClick(ImageTextButton sender , String id)
+        {
+            if(NowSelectDeviceButton != null && NowSelectDeviceButton != sender)
+            {
+                NowSelectDeviceButton.DisActive();
+            }
+
+            NowSelectDeviceButton = sender;
+            NowSelectDeviceID = int.Parse(id);
         }
     }
 }
